d9769209fbd6ffabaf99f8963035dc9255239725
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-06-30 18:35:44 -0400

    Completed the Testing Exercise CH20

--- exercises/CH20/CarApp/Car-Test/UnitTest1.cs
+++ exercises/CH20/CarApp/Car-Test/UnitTest1.cs
@@ -1,13 +1,60 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Car;
+using System;
 
 namespace Car_Test
 {
     [TestClass]
     public class UnitTest1
     {
+        
+        CarClass SampleCar = new CarClass("Nissan","Versa", gasTankSize: 11, milesPerGallon:40,4);
+        
+       
+        //Verifies gas tank level is properly adjusted after using drive method in Car class.
         [TestMethod]
-        public void TestMethod1()
+        public void TestGasTankAfterDriving()
         {
+            SampleCar.Drive(50);
+            Assert.AreEqual(9.75, SampleCar.GasTankLevel);
+        }
+
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            SampleCar.Drive(500);
+            Assert.AreEqual(0, SampleCar.GasTankLevel);
+        }
+
+        //Looks for expected result of ArgumentOutOfRangeException
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverFillException()
+        {
+            SampleCar.AddGas(5);
+            Assert.Fail("Gas limit has been reached!");
+            //SampleCar.GasTankLevel = 20;
+        }
+
+        [TestMethod]
+        public void TestGasTankLevel()
+        {
+            CarClass car1 = new
+            CarClass("Honda", "Accord", 12, 44.5, 4);
+
+            Assert.AreEqual(12, car1.GasTankSize, 1);
+        }
+
+
+        [TestMethod]
+        public void TestGiveMaxDistance()
+        {
+            CarClass car1 = new
+            CarClass("Honda", "Accord", 12, 44.5, 4);
+            double maxDistance = car1.GiveMaxDistance();
+            //Assert.AreEqual(534, maxDistance, 1);
+            Assert.IsFalse(maxDistance > 677);
         }
     }
+    
 }
--- exercises/CH20/CarApp/Car/Car.cs
+++ exercises/CH20/CarApp/Car/Car.cs
@@ -1,10 +1,89 @@
 ﻿using System;
 namespace Car
 {
-    public class Car
+    public class CarClass
     {
-        public Car()
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int GasTankSize { get; set; }
+
+        private double _gasTankLevel;
+        public double GasTankLevel 
+        {   
+            get {return _gasTankLevel; }
+            set 
+            {
+                //if (value > GasTankSize)
+                //{
+                //    throw new ArgumentOutOfRangeException();
+                //}
+                //else
+                //{
+                    _gasTankLevel = value;
+                //}
+            } 
+        }
+        public double MilesPerGallon { get; set; }
+        public double Odometer { get; set; } = 0;
+        public int NumberOfWheels { get; set; }
+
+       
+
+        //Method Updates the GasTankLevel and Odometer based on how many miles driven entered as arg
+        public void Drive(double miles)
+        {
+            //adjust fuel based on mpg and miles requested to drive
+            double maxDistance = MilesPerGallon * GasTankLevel;
+            /**the double below uses some syntax called the ternary operator.
+                * if the value of miles is greater than the value of maxDistance,
+                * then milesAbleToTravel = maxDistance.
+                * otherwise, if miles is not greater than maxDistance,
+                * then milesAbleToTravel = miles
+                */
+            double milesAbleToTravel = miles > maxDistance ? maxDistance : miles;
+            double gallonsUsed = milesAbleToTravel / MilesPerGallon;
+            GasTankLevel -= gallonsUsed;
+            Odometer += milesAbleToTravel;
+        }
+
+        //Method to add gas to the vehicle
+        public void AddGas(double gas)
+        {
+            GasTankLevel += gas;
+            if (GasTankLevel > GasTankSize)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        
+        //Method to provide max distance for vehicle
+        public double GiveMaxDistance()
+        {
+            double maxDistance = this.MilesPerGallon * this.GasTankLevel;
+
+            return maxDistance;
+        }
+
+       
+        //Constructor
+        public CarClass()
         {
         }
+
+        //Constructor overload +1
+        public CarClass(string make, string model, int gasTankSize, double milesPerGallon, int numberOfWheels)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.GasTankSize = gasTankSize;
+            // Gas tank level defaults to a full tank
+            this.GasTankLevel = gasTankSize;
+            this.MilesPerGallon = milesPerGallon;
+            this.NumberOfWheels = numberOfWheels;
+        }
+
+        
     }
+    
 }
